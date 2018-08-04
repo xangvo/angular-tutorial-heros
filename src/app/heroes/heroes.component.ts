31b42e02e1bed7262c5ad17b27732173b2ff63fc
XangVo <vxxang@gmail.com>
2018-08-04 17:08:07 +0700

    add Message component & Message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 
 @Component({
@@ -16,16 +17,25 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
 
 
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes = Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
 }
